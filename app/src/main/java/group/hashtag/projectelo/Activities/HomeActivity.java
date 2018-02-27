3d46d756cd372a82d26ae2b28794cd26bc5436bf
Nikhil Kamath <nikhilvk91@gmail.com>
2018-02-27 15:32:12 -0400

    Initial Commit

---
+++ app/src/main/java/group/hashtag/projectelo/Activities/HomeActivity.java
@@ -0,0 +1,19 @@
+package group.hashtag.projectelo.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import group.hashtag.projectelo.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
