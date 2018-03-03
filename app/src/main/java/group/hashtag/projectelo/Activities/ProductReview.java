bc0f1f706e40debe38c162aa065b7cee69d956d3
Aman Tewary <aman.tewary@live.com>
2018-03-02 22:28:11 -0400

    Added Layout and Activity for Product Review

---
+++ app/src/main/java/group/hashtag/projectelo/Activities/ProductReview.java
@@ -0,0 +1,36 @@
+package group.hashtag.projectelo.Activities;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import group.hashtag.projectelo.R;
+
+public class ProductReview extends AppCompatActivity {
+    TextView title;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.review_layout);
+        title = findViewById(R.id.title_toolbar);
+        Typeface ReemKufi_Regular = Typeface.createFromAsset(getAssets(), "fonts/ReemKufi-Regular.ttf");
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        title.setTypeface(ReemKufi_Regular);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_arrow_back_black_24dp));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
