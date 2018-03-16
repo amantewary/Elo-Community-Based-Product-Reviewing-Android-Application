284b8e179beb6a881853967829dc4e3b04bc4280
Aman Tewary <aman.tewary@live.com>
2018-03-16 18:53:37 -0300

    Parsing Category in Wishlist. Changed Wishlist Add and View Layout.

--- app/src/main/java/group/hashtag/projectelo/Activities/WishlistAddItem.java
+++ app/src/main/java/group/hashtag/projectelo/Activities/WishlistAddItem.java
@@ -4,18 +4,25 @@ import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import group.hashtag.projectelo.R;
 
@@ -30,7 +37,9 @@ public class WishlistAddItem extends AppCompatActivity {
     EditText wlAddDeviceName;
     Spinner wlCatSpinner;
 
+    Map<String, Object> mapCategories;
     List<String> listCategories;
+    List<Map<String, Object>> listMapCategories;
     ArrayAdapter<String> categories;
 
     DatabaseReference categoriesRef;
@@ -48,7 +57,10 @@ public class WishlistAddItem extends AppCompatActivity {
         wlAddDeviceName = findViewById(R.id.wlAddDeviceName);
         wlCatSpinner = findViewById(R.id.wlCatSpinner);
 
+        listCategories = new ArrayList<>();
+        listMapCategories = new ArrayList<>();
 
+        fetchCategories(categoriesRef);
         title.setTypeface(ReemKufi_Regular);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
@@ -60,7 +72,23 @@ public class WishlistAddItem extends AppCompatActivity {
             }
         });
 
+        categories = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, listCategories);
+        wlCatSpinner.setAdapter(categories);
+        wlCatSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
 
+                }else{
+                    Log.e(NewReviewActivity.class.getCanonicalName(), "Position" + position);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 
@@ -78,5 +106,27 @@ public class WishlistAddItem extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void fetchCategories(DatabaseReference categoriesRef) {
+        categoriesRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listCategories.add(0, "Select Category");
+                for (DataSnapshot dsnp : dataSnapshot.getChildren()) {
+                    mapCategories = (Map<String, Object>) dsnp.getValue();
+                    String value = mapCategories.get("CatName").toString();
+                    listCategories.add(value);
+                    listMapCategories.add(mapCategories);
+                }
+                Log.e("Here", "" + listMapCategories);
+                categories.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("Here", "" + databaseError);
+            }
+        });
     }
 }
