bddb6b6c1bebe97aa8332819530281e55188abc3
Mokshh <moksh@dal.ca>
2018-03-26 20:03:18 -0400

    Added layout for User detail form.

---
+++ app/src/main/java/group/hashtag/projectelo/Activities/ProfileSetup.java
@@ -0,0 +1,15 @@
+package group.hashtag.projectelo.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import group.hashtag.projectelo.R;
+
+public class ProfileSetup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_setup);
+    }
+}
---
+++ app/src/main/java/group/hashtag/projectelo/Activities/ProfileSetupCont.java
@@ -0,0 +1,17 @@
+package group.hashtag.projectelo.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import group.hashtag.projectelo.R;
+
+public class ProfileSetupCont extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_setup);
+
+
+    }
+}
