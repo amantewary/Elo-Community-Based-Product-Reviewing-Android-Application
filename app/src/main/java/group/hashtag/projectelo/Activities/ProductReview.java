c601c2554604321ff33dc0d5ce842ca091d806b7
Nikhil Kamath <nikhilvk91@gmail.com>
2018-03-30 12:15:52 -0300

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/group/hashtag/projectelo/Activities/ProductReview.java
+++ app/src/main/java/group/hashtag/projectelo/Activities/ProductReview.java
@@ -34,8 +34,8 @@ import java.util.List;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import group.hashtag.projectelo.Activities.OtherUserActivities.ViewUserProfile;
 import group.hashtag.projectelo.Handlers.CommentHandler;
-import group.hashtag.projectelo.Handlers.WishlistItem;
 import group.hashtag.projectelo.R;
 
 public class ProductReview extends AppCompatActivity {
